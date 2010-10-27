412f2979cf36a24ecb027a14aeba57cb95884ded
Emad Mokhtar <emad.m.habib@gmail.com>
2010-10-27 12:54:15 +0300

    First Commit

---
+++ GoogleTranslationCS/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Google.API.Translate;
+
+namespace GoogleTanslationCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TranslateClient client = new TranslateClient("www.emadmokhtar.com");
+            string tranlsated = client.Translate("أهلا و سهلا", "ar", "en");
+            Console.WriteLine(tranlsated); //Welcome
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GoogleTranslationVB/Module1.vb
@@ -0,0 +1,11 @@
+﻿Imports Google.API.Translate
+Module Module1
+
+    Sub Main()
+        Dim client As New TranslateClient("www.emadmokhtar.com")
+        Dim translated As String = client.Translate("أهلا و سهلا", "ar", "en")
+        Console.WriteLine(translated) 'Welcome
+        Console.ReadLine()
+    End Sub
+
+End Module
